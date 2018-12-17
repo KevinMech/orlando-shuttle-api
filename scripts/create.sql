23d7c9d169d0d2b077d2a6822c3f26c8ec0ea5fa
KevinMech <null>
2018-12-17 17:09:22 -0500

    Add SQL create script

---
+++ scripts/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE buses(
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE stops(
+    id          INTEGER REFERENCES buses(id),
+    longitude   DECIMAL NOT NULL,
+    latitude    DECIMAL NOT NULL
+);
+
+CREATE TABLE routes(
+    id          INTEGER REFERENCES buses(id),
+    longitude   DECIMAL NOT NULL,
+    latitude    DECIMAL NOT NULL
+);
