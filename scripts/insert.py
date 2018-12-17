fc714f8398d850f88d7a312576a7d7efff0a87b8
KevinMech <null>
2018-12-17 18:16:10 -0500

    Add connection function to insert script

---
+++ scripts/insert.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import psycopg2
+
+    
+def connect():
+    try:
+        if len(sys.argv) > 1 and sys.argv[1].lower() == '-debug':
+            conn = psycopg2.connect('dbname=busroutedb user=postgres')
+        else:
+            conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+        cur = conn.cursor()
+        cur.execute('SELECT version()')
+        version = cur.fetchone()
+        print('Connected to PostgreSQL database!')
+        print(version[0])
+        print('-------------------------------------------------------------------')
+        return conn
+    except (psycopg2.DatabaseError) as error:
+        print('Connection Error:')
+        print(error)
+        sys.exit()
+
+if __name__ == "__main__":
+    conn = connect()
