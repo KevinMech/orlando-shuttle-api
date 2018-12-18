5c509bad20c5e4cf605cad4c1e2492eb1de7241e
KevinMech <kevin.mellachew@gmail.com>
2018-12-18 14:41:41 -0500

    Refactor functions to fit PEP-8 standards

--- scripts/insert.py
+++ scripts/insert.py
@@ -3,39 +3,43 @@ import os
 import json
 import psycopg2
 
-json_directory = '../geojson/'
-    
-def connect():
+JSON_DIRECTORY = '../geojson/'
+
+
+def db_connect():
     try:
         if len(sys.argv) > 1 and sys.argv[1].lower() == '-debug':
             conn = psycopg2.connect('dbname=busroutedb user=postgres')
         else:
-            conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+            env = os.environ['DATABASE_URL']
+            conn = psycopg2.connect(env, sslmode='require')
         cur = conn.cursor()
         cur.execute('SELECT version()')
         version = cur.fetchone()
         print('Connected to PostgreSQL database!')
         print(version[0])
-        print('-------------------------------------------------------------------')
+        print('--------------------------------------------------------------')
         return conn
     except (psycopg2.DatabaseError) as error:
         print('Connection Error:')
         print(error)
         sys.exit()
 
+
 def parse_file(file):
     print('parsing ' + file)
-    with open(json_directory + file) as file:
+    with open(JSON_DIRECTORY + file) as file:
         geojson = json.load(file)
-    return geojson
+    return geojson['features']
 
-def read_files():
+
+def poppulate_files():
     print('Reading files...')
-    for filename in os.listdir(json_directory):
+    for filename in os.listdir(JSON_DIRECTORY):
         if filename.endswith('.json'):
             geojson = parse_file(filename)
-
+            print(geojson)
 
 if __name__ == "__main__":
-    conn = connect()
-    read_files()
+    conn = db_connect()
+    poppulate_files()
