038b9162157be9663875f5df44a068cebe1ac729
KevinMech <kevin.mellachew@gmail.com>
2018-12-21 16:56:32 -0500

    Add docstrings

--- scripts/insert.py
+++ scripts/insert.py
@@ -7,6 +7,13 @@ JSON_DIRECTORY = '../geojson/'
 
 
 def db_connect():
+    """
+    Open and test connection to the database
+        Use -debug flag to connect to the localhost database
+    
+        Returns: 
+        conn (object): Connection object used to manage connection with database
+    """
     try:
         if len(sys.argv) > 1 and sys.argv[1].lower() == '-debug':
             conn = psycopg2.connect(database="busroutedb", user="postgres")
@@ -19,6 +26,7 @@ def db_connect():
         print('Connected to PostgreSQL database!')
         print(version[0])
         print('--------------------------------------------------------------')
+        print(conn)
         return conn
     except (psycopg2.DatabaseError) as error:
         print('Connection Error:')
@@ -27,8 +35,14 @@ def db_connect():
 
 
 def parse_file(file, cur):
+    """
+    Parse for values of JSON files to insert into database
+        Parameters:
+        file (string): Filename used to reference read file
+        cur (object): Cursor object used to execute SQL commands in psycopg2
+    """
     id = None
-    print('parsing ' + file + '...')
+    print('Parsing ' + file + '...')
 
     with open(JSON_DIRECTORY + file) as file:
         geojson = json.load(file)
@@ -52,40 +66,76 @@ def parse_file(file, cur):
                 for coords in features['geometry']['coordinates']:
                     db_insert_stop_route(id, 'routes', coords[1], coords[0], cur)
         print('Successfully added to database!')
-    
+
 
 def db_insert_name(name, cur):
+    """
+    Insert route name into the database and return its corresponding ID
+
+        Parameters:
+        name (string): Filename used to reference file
+        cur (object): Cursor object used to execute SQL commands in psycopg2
+        
+        Returns:
+        id (int): Integer representing the ID of the bus name
+    """
     print('Inserting ' + name + ' into database...')
     try:
         cur.execute("INSERT INTO buses (name) VALUES ('{0}');".format(name))
         cur.execute("SELECT id FROM buses WHERE name ='{0}';".format(name))
         return cur.fetchone()
     except (Exception, psycopg2.DatabaseError) as err:
-        print('Failed to write ' + name + 's name to database!')
+        print('[Error] Failed to write ' + name + 's name to database')
         print(err)
 
 
 def db_insert_stop_route(id, table, longitude, latitude, cur):
+    """
+    Insert coordinates into either the stop or route tables in the database
+        Parameters:
+        id (int): Integer representing the ID of the bus name. Used for foriegn key
+        table (string): Represents the table to insert data into
+        longitude (float): Longitude coordinates of the route or stop
+        latitude (float): Latitude coordinates of the route or stop
+        cur (object): Cursor object used to execute SQL commands in psycopg2
+    """
     try:
         cur.execute("INSERT INTO {0} (id, longitude, latitude) VALUES ('{1}', '{2}', '{3}');".format(table, id[0], longitude, latitude))
     except (Exception, psycopg2.DatabaseError) as err:
-        print('Failed to write' + table + 'to database!')
+        print('[Error] Failed to write' + table + 'to database')
         print(err)
 
 
-def poppulate_files(cur):
+def read_files(cur):
+    """
+    Read all files in specified directory and then send to parse function to be parsed and inserted
+        Parameters:
+        cur (object): Cursor object used to execute SQL commands in psycopg2
+    """
     print('Reading files...')
     for filename in os.listdir(JSON_DIRECTORY):
         if filename.endswith('.json'):
-            geojson = parse_file(filename, cur)
+            parse_file(filename, cur)
 
 
 def reset_tables(cur):
-    cur.execute("TRUNCATE TABLE buses RESTART IDENTITY CASCADE;")
-    
+    """
+    Remove all data from all tables for rewriting purposes
+        Parameters:
+        cur (object): Cursor object used to execute SQL commands in psycopg2
+    """
+    print('Resetting tables in database...')
+    try:
+        cur.execute("TRUNCATE TABLE buses RESTART IDENTITY CASCADE;")
+        print("Reset Complete!")
+    except (Exception, psycopg2.DatabaseError) as err:
+        print('[Error] Failed to reset database')
+
+
 if __name__ == "__main__":
     conn = db_connect()
     cur = conn.cursor()
     reset_tables(cur)
-    poppulate_files(cur)
+    read_files(cur)
     conn.commit()
+    conn.close()
