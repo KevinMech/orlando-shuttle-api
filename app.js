010b9cfe3857a881fa6d4f5f34c37cfb887ad1e1
KevinMech <kevin.mellachew@gmail.com>
2018-12-27 17:41:47 -0500

    Add eslint config file

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) =>{
-    res.send('<h3>Welcome!</h3>' +
-    '<p>This page is still under construction.</p>')
+app.get('/', (req, res) => {
+    res.send('<h3>Welcome!</h3>'
+    + '<p>This page is still under construction.</p>');
 });
 
-app.listen(port, (req, res)=>{
-    console.log(`Successfully connected to port ${port}`)
+app.listen(port, () => {
+    console.log(`Successfully connected to port ${port}`);
 });
