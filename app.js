f318115cb5f98787eeb1adf7b6f57a877b2b41df
KevinMech <kevin.mellachew@gmail.com>
2018-12-27 14:10:20 -0500

    Add root route method

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.listen(port, ()=>{
-    console.log("success!")
+app.get('/', (req, res) =>{
+    res.send('<h3>Welcome!</h3>' +
+    '<p>This page is still under construction.</p>')
+});
+
+app.listen(port, (req, res)=>{
+    console.log(`Successfully connected to port ${port}`)
 });
