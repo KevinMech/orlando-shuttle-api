6e2969a0f3f87965e5d0506d5a947740eb3aec71
KevinMech <kevin.mellachew@gmail.com>
2018-12-30 15:05:29 -0500

    Add id query functionality

--- routes/buses.js
+++ routes/buses.js
@@ -39,7 +39,7 @@ router.get('/shuttle', async (req, res) => {
     res.send(shuttles);
 });
 
-router.get('/shuttle/:name', async (req, res) => {
+router.get('/shuttle/name/:name', async (req, res) => {
     let stops;
     let routes;
     for (const shuttle of availableshuttles) {
@@ -56,7 +56,32 @@ router.get('/shuttle/:name', async (req, res) => {
             return;
         }
     }
-    res.status(404).send("404 resource not found");
+    res.status(404).send('404 resource not found');
+});
+
+router.get('/shuttle/id/:id', async (req, res) => {
+    let stops;
+    let routes;
+    const id = Number(req.params.id);
+    if (!Number.isNaN(id)) {
+        for (const shuttle of availableshuttles) {
+            if (id === shuttle.id) {
+                try {
+                    stops = await db.getStops(shuttle.id);
+                    routes = await db.getRoutes(shuttle.id);
+                } catch (err) {
+                    console.log(`Error: Failed to fetch JSON for shuttle ${shuttle.name}`);
+                    console.log(err);
+                }
+                const shuttleJSON = new Shuttle(shuttle.id, shuttle.name, stops, routes);
+                res.send(shuttleJSON);
+                return;
+            }
+        }
+        res.status(404).send('404 resource not found');
+    } else {
+        res.status(400).send('400 bad request');
+    }
 });
 
 module.exports = router;
